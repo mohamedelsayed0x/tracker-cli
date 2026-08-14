6ae0768547bafd49f3a62b891a352ad1ab5f571e
Mohamed Elsayed <moelsayko@gmail.com>
2026-08-14 23:16:23 +0300

    Make Structure in the program

--- obj/Debug/net10.0/CLI Application.AssemblyInfo.cs
+++ obj/Debug/net10.0/CLI Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CLI Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fff645193334dc823270aeebf08026e6f7329cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("CLI Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CLI Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
