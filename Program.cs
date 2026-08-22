9768c6bf881e7889edfebfed91033260cde20bc0
Mohamed Elsayed <moelsayko@gmail.com>
2026-08-22 23:17:34 +0300

    feat: add switch statement to handle CLI commands

--- Program.cs
+++ Program.cs
@@ -8,3 +8,41 @@ if (args.Length == 0)
   ShowHelp();
   return;
 }
+string command = args[0].ToLower();
+
+switch (command)
+{
+  case "add":
+    // AddTask();
+    break;
+
+  case "update":
+    // UpdateTask();
+    break;
+
+  case "delete":
+    // DeleteTask();
+    break;
+
+  case "mark-in-progress":
+    // MarkInProgress();
+    break;
+
+  case "mark-done":
+    // MarkDone();
+    break;
+
+  case "list":
+    // ListTasks();
+    break;
+
+  case "help":
+    // ShowHelp();
+    break;
+
+  default:
+    Console.WriteLine($"Unknown command: '{args[0]}'.");
+    Console.WriteLine();
+    // ShowHelp();
+    break;
+}
